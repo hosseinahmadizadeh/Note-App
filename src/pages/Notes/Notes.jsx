7cde137b52306f8141d4a90a74d04cc13a7d5e6b
hosseinahmadizadeh <hosseinahmadizadeh1377@gmail.com>
2023-10-18 19:34:33 +0330

    Search note functionality

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NoteItem from "../../components/NoteItem/NoteItem";
 import { BsSearch } from "react-icons/bs";
 import { BsPlusLg } from "react-icons/bs";
 import { Link } from "react-router-dom";
 const Notes = ({ notes, setNotes }) => {
+  const [text, setText] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState(notes);
+
   const handleDeleteNote = (id) => {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
   };
+
+  const handleSearch = () => {
+    setFilteredNotes(
+      notes.filter((note) => {
+        if (
+          note.title.toLowerCase().match(text.toLocaleLowerCase()) ||
+          note.content.toLowerCase().match(text.toLocaleLowerCase())
+        ) {
+          return note;
+        }
+      })
+    );
+  };
+
+  useEffect(handleSearch, [text]);
 
   return (
     <div className="bg-gradient-to-r from-slate-900 to-rose-500 h-screen pb-3">
@@ -19,6 +37,11 @@ const Notes = ({ notes, setNotes }) => {
             type="text"
             placeholder="Keyword..."
             autoFocus
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+              handleSearch();
+            }}
           />
           <button className="bg-rose-400 rounded-md p-3">
             <BsSearch size={25} color="white" />
@@ -36,7 +59,7 @@ const Notes = ({ notes, setNotes }) => {
         </Link>
       </header>
       <section className="grid grid-cols-3 gap-4 m-3">
-        {notes.map((note) => (
+        {filteredNotes.map((note) => (
           <NoteItem
             key={note.id}
             note={note}
